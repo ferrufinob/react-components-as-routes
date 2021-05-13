adcce53f1405c0fd11c226fe319dadbaf65e9dce
Brenda Ferrufino <ferrufinob@gmail.com>
2021-05-12 22:11:38 -0400

    commit tree 4f6512ca29c620e99578fce3b5e37801855e08fc

---
+++ src/Messages.js
@@ -0,0 +1,17 @@
+import React, { Component } from "react";
+
+export default class Messages extends Component {
+  render() {
+    return (
+      <>
+        <h1>Messages</h1>
+        <ul>
+          <li>Message 1</li>
+          <li>Message 2</li>
+          <li>Message 3</li>
+          <li>Message 4</li>
+        </ul>
+      </>
+    );
+  }
+}
--- src/index.js
+++ src/index.js
@@ -1,15 +1,23 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-
-const Home = () => {
-  return (
-    <div>
-      <h1>Home!</h1>
-    </div>
-  );
-};
+import React from "react";
+import ReactDOM from "react-dom";
+import Home from "./Home";
+import About from "./About";
+import Login from "./Login";
+import Signup from "./Signup";
+import Messages from "./Messages";
+import Navbar from "./Navbar";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 
 ReactDOM.render(
-  <Home />,
-  document.getElementById('root')
+  <Router>
+    <>
+      <Navbar />
+      <Route exact path="/" component={Home} />
+      <Route path="/about" component={About} />
+      <Route path="/login" component={Login} />
+      <Route path="/signup" component={Signup} />
+      <Route path="/messages" component={Messages} />
+    </>
+  </Router>,
+  document.getElementById("root")
 );
